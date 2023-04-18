677290c042e72783dfc08e73ff6898794ec5eea2
Sirimas2828 <biwsiri.k@gmail.com>
2023-04-18 21:03:10 +0700

    edit table

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -18,7 +18,7 @@ function Owner() {
         <div className="container">
           <h1>Biw No.43 From Group J</h1>
           <img width= '470px' src="https://cdn.pixabay.com/photo/2017/11/14/13/06/kitty-2948404_960_720.jpg" />
-          <h4>Short Biography: </h4>
+          <h3>Short Biography: </h3>
           <p>
             Hello, I'm learner from Generation. Nice to meet you. I am a flower in this picture and React is a cat which try to eat me alive. LOL XD
           </p>
